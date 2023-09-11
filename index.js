8b3d95474e36d5fcfbbb5ad1bbc2305e705a1482
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-09-11 22:54:32 +0545

    feat:Working on call accepted

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ io.on("connection", (socket) => {
     const { email, room } = data;
     emailToSocketIdMap.set(email, socket.id);
     socketIdToEmailMap.set(socket.id, email);
-    io.to(socket.id).emit('room:join',data);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+    socket.on("user:call", ({ to, offer }) => {
+      io.to(to).emit("incomming:call", { from: socket.id, offer });
+    });
+    socket.on("call:accepted", ({ to, ans }) => {
+      io.to(to).emit("call:accepted", { from: socket.id, ans });
+    });
   });
 });
